d09c59e7324d067ee9fc0f5e15a03f7a4fdfc7f7
SoulDrake <maxsouldrake@gmail.com>
2021-11-20 11:57:51 +0100

    add postgres db

---
+++ src/main/resources/database/populateDB.sql
@@ -0,0 +1,24 @@
+DELETE FROM films;
+
+INSERT INTO films (title, year, genre, date, actors, description, poster, country, priority)
+VALUES
+    ('film 1', 2001, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5),
+    ('film 2', 2002, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5),
+    ('film 3', 2003, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5),
+    ('film 4', 2004, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5),
+    ('film 5', 2005, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5),
+    ('film 6', 2006, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5),
+    ('film 7', 2007, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5),
+    ('film 8', 2008, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5),
+    ('film 9', 2009, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5),
+    ('film 10', 2010, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5),
+    ('film 11', 2011, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5),
+    ('film 12', 2012, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5),
+    ('film 13', 2013, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5),
+    ('film 14', 2014, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5),
+    ('film 15', 2015, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5),
+    ('film 16', 2016, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5),
+    ('film 17', 2017, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5),
+    ('film 18', 2018, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5),
+    ('film 18', 2019, 'fantasy', '2019-01-14', '1,2,3,4,5', 'description', '328', 'USA', 5);
+
