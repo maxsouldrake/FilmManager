8745b6cdabf5188fed6d27fce7a67dfbbe4ae101
SoulDrake <maxsouldrake@gmail.com>
2021-12-06 15:03:34 +0100

    change names of mutable constants

--- src/test/java/org/souldrake/filmmanager/TestData.java
+++ src/test/java/org/souldrake/filmmanager/TestData.java
@@ -17,19 +17,19 @@ public class TestData {
     public static final int FILM_ID = 1;
     public static final int NOT_FOUND_ID = 30;
 
-    public static final Film FILM1 = new Film(FILM_ID, "title1", 2001, "genre1", "country1",
+    public static final Film film1 = new Film(FILM_ID, "title1", 2001, "genre1", "country1",
             LocalDate.of(2011, 1, 1), (byte) 1, "actors1", "description1", "posterUrl1");
-    public static final Film FILM2 = new Film(FILM_ID + 1, "title2", 2002, "genre2", "country2",
+    public static final Film film2 = new Film(FILM_ID + 1, "title2", 2002, "genre2", "country2",
             LocalDate.of(2012, 2, 2), (byte) 2, "actors2", "description2", "posterUrl2");
-    public static final Film FILM3 = new Film(FILM_ID + 2, "title3", 2003, "genre3", "country3",
+    public static final Film film3 = new Film(FILM_ID + 2, "title3", 2003, "genre3", "country3",
             LocalDate.of(2013, 3, 3), (byte) 3, "actors3", "description3", "posterUrl3");
-    public static final Film FILM4 = new Film(FILM_ID + 3, "title4", 2004, "genre4", "country4",
+    public static final Film film4 = new Film(FILM_ID + 3, "title4", 2004, "genre4", "country4",
             LocalDate.of(2014, 4, 4), (byte) 4, "actors4", "description4", "posterUrl4");
-    public static final Film FILM5 = new Film(FILM_ID + 4, "title5", 2005, "genre5", "country5",
+    public static final Film film5 = new Film(FILM_ID + 4, "title5", 2005, "genre5", "country5",
             LocalDate.of(2015, 5, 5), (byte) 5, "actors5", "description5", "posterUrl5");
 
 
-    public static final List<Film> FILMS = Arrays.asList(FILM1, FILM2, FILM3, FILM4, FILM5);
+    public static final List<Film> films = Arrays.asList(film1, film2, film3, film4, film5);
 
     public static Film getNew() {
         return new Film(null, "newTitle", 1999, "newGenre", "newCountry", LocalDate.of(2009, 12, 12),
@@ -37,8 +37,8 @@ public class TestData {
     }
 
     public static Film getUpdated() {
-        Film updated = new Film(FILM_ID, FILM1.getTitle(), FILM1.getYear(), FILM1.getGenre(), FILM1.getCountry(),
-                FILM1.getDate(), FILM1.getPriority(), FILM1.getActors(), FILM1.getDescription(), FILM1.getPosterUrl());
+        Film updated = new Film(FILM_ID, film1.getTitle(), film1.getYear(), film1.getGenre(), film1.getCountry(),
+                film1.getDate(), film1.getPriority(), film1.getActors(), film1.getDescription(), film1.getPosterUrl());
         updated.setTitle("updatedTitle");
         updated.setYear(2555);
         updated.setGenre("updGenre");
